46c7c69bfc3d4f9f346f3aac186a507aae804b24
Dhruv278 <dhruvgopani8@gmail.com>
2023-12-20 18:27:39 +0530

    change redirect url

--- public/index.js
+++ public/index.js
@@ -5,7 +5,7 @@ const pushData = async (name, data) => {
         const productData = JSON.parse(data);
         const storename = prompt("Enter your Shopify store name: ");
 
-        const url = `http://localhost:3000/api/shopify/addProduct/${storename}`;
+        const url = `https://shopify-app-teal.vercel.app/api/shopify/addProduct/${storename}`;
         const options = {
             method: 'POST',
             headers: {
